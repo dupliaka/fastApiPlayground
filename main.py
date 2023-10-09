9948a9ef510750623557844768a78ab9d12cd9b3
adupliak <dupliaka@gmail.com>
2023-10-09 21:02:46 +0200

    Upload image

--- main.py
+++ main.py
@@ -1,19 +1,20 @@
-from fastapi import FastAPI
 from fastapi.testclient import TestClient
+from fastapi import FastAPI,  UploadFile
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+@app.post("/uploadfile/")
+async def create_upload_file(file: UploadFile):
+    return {"filename": file.filename}
+
 
 client = TestClient(app)
 
 
-def test_read_main():
-    response = client.get("/")
+def test_upload_image():
+    files = {'file': ('nekroxmas.jpg', open("nekroxmas.jpg", 'rb'))}
+    response = client.post("/uploadfile/", files=files)
     assert response.status_code == 200
-
-
+    assert response.json() == {"filename": "nekroxmas.jpg"}
 
